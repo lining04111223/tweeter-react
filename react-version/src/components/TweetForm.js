e5e200e1650b5802cd14dd1fbfc0cc564931ad0d
马琳娜 <malinna@malinnadeMacBook-Pro.local>
2022-10-30 21:08:37 -0700

    react practice with tweeter

---
+++ react-version/src/components/TweetForm.js
@@ -0,0 +1,15 @@
+function TweetForm(){
+  const placeholder ="What are you humming about?"
+  return (
+    <section className="newtweet">
+      <form onSubmit={(e) =>{e.preventDefault()}}
+      method="post" action="/tweets" className="newtweet__form">
+        <textarea className="form__textarea" name="text" placeholder={placeholder}></textarea>
+        <input type="submit" value="Tweet" className="form__input" />
+        <span className="form__counter">140</span>
+      </form>
+    </section>
+  );
+}
+
+export default TweetForm;
